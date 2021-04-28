d2ac9c25e39a811514029d7087a9eff7b1a6876d
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-04-28 21:22:53 +0900

    login page, connecting mysql

--- chat.js
+++ chat.js
@@ -4,9 +4,23 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
+const jwt = require("jsonwebtoken");
+let mysql = require('mysql');
+
+
+let connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '6867',
+    database : 'mydb'
+})
+
+connection.connect();
 
 app.use(express.static('public')); // 여기 폴더 안에 있는 파일들만 외부로 공개. 웬만하면 하나만.
 // 왜 css파일들이 /assets으로 되어있는데 되나? 그것은. root위에 public폴더를 올린 것이 아니라, root위에 public한 폴더들을 바로 연결해주었기 때문.
+
+app.use(express.json());
 
 /* 
 app.get('/', (request, response)=>{
@@ -17,9 +31,59 @@ app.get('/', (request, response)=>{
 */
 
 app.get('/',(request, response)=>{
+    response.sendFile(__dirname + '/login.html')
+})
+
+app.get('/chatroom',(request, response)=>{
     response.sendFile(__dirname + '/chatroom.html');
 });
+
+app.post('/signup',(request, response)=>{
+    console.log(request.body);
+    var userName = req.body.
+});
+
+app.post('/login', function(req, res){
+    console.log("사용자 입력정보 : ", req.body);
+    var userEmail = req.body.userEmail;
+    var userPassword = req.body.userPassword;
+    //db 가서 페스워드 맞는지 체크
+    var sql = "SELECT * FROM WHERE email = ?";
+    connection.query(sql, [userEmail], function(error, results, fields){
+        if (error) throw error;
+        else {
+            if (results.length === 0){
+                res.json("등록되지 않은 아이디입니다.");
+            } else {
+                var dbPassword = results[0].user_password;
+                if (userPassword == dbPassword){
+                    var tokenkey = "fintech";
+                    jwt.sign(
+                        {
+                            userId : results[0].user_id,
+                            userEmail: results[0].user_email
+                        }, tokenkey,
+                        {
+                            expiresIn : "10d",
+                            issuer : "fintech.admin",
+                            subject : "user.login.info",
+                        }, 
+                        function(err,token) {
+                            console.log("로그인 성공", token);
+                            res.json(token);
+                        }
+                    );
+                } else {
+                    res.json("비밀번호가 다릅니다.");
+                }
+
+            }
+        }
+    })
+})
+
 var userList = [];
+let highPrice = 0;
 
 // 서버 쪽에서 ~.on 은 'event' 발생시에 작업. emit 'event' 건내주는 것.
 
@@ -30,10 +94,34 @@ io.on('connection',(socket)=>{
         // io가 socket보다 큰 범위 인듯. InputOutput은 모든 연결, socket은 개별 연결!!!
         console.log(socket.id);
     });
+
+    /*
+    입찰 소켓 온
+    {
+        현재 최고가 비교 
+        최고가보다 높다면 갱신
+        emit 으로 전체사용자에게 전달
+    }
+    */
+
+
     socket.on('chat message',(msg)=>{
+        msg = parseInt(msg);
         console.log(`message : ${msg}`);
-        io.emit('chat message',msg); // 다시 client 쪽으로 쏴주기  Broadcasting 알려주는 것.
+        io.emit('chat message',msg);
+
+        console.log(`highprice : ${highPrice}`)
+        if(msg > highPrice){
+            highPrice = msg;
+            io.emit('highest price',msg);
+        }
+       
+        // 다시 client 쪽으로 쏴주기  Broadcasting 알려주는 것.
     });
+
+
+
+
     socket.on('login',(msg)=>{
         console.log(`login : ${msg}`);
         userList.map((user) => {
@@ -58,8 +146,21 @@ server.listen(3000,()=>{
     console.log("listening on * :3000");
 })
 
+// SQL 파트. mysql table은 원래 소문자랑 _ 로 써야함. 
+/*    
+//let sql = 'SELECT * FROM product';
+let sql = "INSERT user(SocketID, UserID, UserPassword, ProductID, UserAccount)VALUES('12345678901234567890','test','1234','','AMBO12341234')";
 
+connection.query(sql,(error, results, fields)=>{  //results는 행의 정보 담은 배열, fields는 컬럼
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+})
 
+connection.end();
+
+*/
 
 /* 
 화요일. 오늘 구현해야 할 서비스.
